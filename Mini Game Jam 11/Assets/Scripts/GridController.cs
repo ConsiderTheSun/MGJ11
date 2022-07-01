0dac2b52cee9e2b01f44922d52c1ed1765dc818e
neptune7000 <34799539+neptune7000@users.noreply.github.com>
2022-07-01 17:56:36 -0400

    Files

--- Mini Game Jam 11/Assets/Scripts/GridController.cs
+++ Mini Game Jam 11/Assets/Scripts/GridController.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour{
-	[Header("Set in Inspector")]
-	public PlayerController player;
-	public GridController grid;
-
+public class GridController : MonoBehaviour{
 	// Start is called before the first frame update
 	void Start(){
-		grid.LoadMap();
+
 	}
 
 	// Update is called once per frame
 	void Update(){
-		
+
 	}
 
-	void FixedUpdate(){
-		player.FixedUpdatePlayer();
+
+	public void LoadMap(){
 
 	}
 }
