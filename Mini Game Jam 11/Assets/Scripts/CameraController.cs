3b58f69f6e1cda6751f990cd961625b4d967e56c
freddie! <33379024+ShayLycan@users.noreply.github.com>
2022-07-02 11:56:29 -0400

    Updated Camera Controller
    smoothed camera movement using Vector3 lerp

--- Mini Game Jam 11/Assets/Scripts/CameraController.cs
+++ Mini Game Jam 11/Assets/Scripts/CameraController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour{
-
+public class CameraController : MonoBehaviour
+{
 	[Header("Set in Inspector")]
 	public Transform playerTransform;
+	public Vector3 cameraOffset;
+	public float cameraSpeed = 0.1f;
 	// Start is called before the first frame update
-	void Start(){
-
+	
+	void Start()
+	{
+		transform.position = playerTransform.position + cameraOffset;
 	}
 
+	void FixedUpdate ()
+    {
+		Vector3 finalPosition = playerTransform.position + cameraOffset;
+		Vector3 lerpPosition = Vector3.Lerp(transform.position, finalPosition, cameraSpeed);
+		transform.position = lerpPosition;
+    }
+	// lerpPosition is the current camera position
+
+
+/* i believe this is unneeded with vector3 lerp added ~fredddie
+ * 
 	// Update is called once per frame
 	void Update(){
 		transform.position = new Vector3(playerTransform.position.x,playerTransform.position.y, -10);
 	}
 }
+*/
